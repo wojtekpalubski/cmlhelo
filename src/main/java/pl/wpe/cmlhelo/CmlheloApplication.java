ebb8e17d3059a5994ca48a372fd040d11510068c
qq <qq>
2017-10-21 14:22:11 +0200

    wygenerowany projekt

---
+++ src/main/java/pl/wpe/cmlhelo/CmlheloApplication.java
@@ -0,0 +1,12 @@
+package pl.wpe.cmlhelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmlheloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmlheloApplication.class, args);
+	}
+}
