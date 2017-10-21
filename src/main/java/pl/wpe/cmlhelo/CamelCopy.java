a22ea00e7cf622061e6753924f597f6ebf745654
qq <qq@wp>
2017-10-21 16:31:04 +0200

    dodanie timera

---
+++ src/main/java/pl/wpe/cmlhelo/CamelCopy.java
@@ -0,0 +1,19 @@
+package pl.wpe.cmlhelo;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CamelCopy extends RouteBuilder {
+
+    @Override
+    public void configure() {
+//        from("file://plikwe?noop=true").to("file://plikwy");
+        from("timer:foo?period=5000")
+                .to("log:bar");
+    }
+
+}
+
+
